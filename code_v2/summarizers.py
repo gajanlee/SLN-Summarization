4707082f884fde00c479628fa406a0b69fec7770
gajanlee <lee_jiazh@163.com>
2020-11-30 17:20:26 +0800

    add summarizer and experiment framework

---
+++ code_v2/summarizers.py
@@ -0,0 +1,29 @@
+from copy import deepcopy
+
+def SLN_summarizer(sentences, SLNs, word_score_dict):
+    assert len(SLNs) == len(sentences)
+
+    
+
+def _sentence_selection(sentences, SLNs, word_score_dict):
+    pass
+
+
+def _sentence_simplification(sentence, word_score_dict, threshold):
+    pass
+
+
+def _check_equivalence():
+
+    return -1
+
+    return new_threshold
+
+
+def _decrease_redundancy(selected_sentence, word_score_dict):
+    word_score_dict = deepcopy(word_score_dict)
+    pass
+
+
+def _increase_redundancy(selected_sentence, SLNs, word_score_dict):
+    pass
